0254e339e9c2b9125caf4bbfda5fcd8217bbd9e4
retroazure <tomascosta005@outlook.com>
2020-09-22 17:47:50 +0100

    Dealing with routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const App = () => {
+
   return (
-    <>
+    <Router>
+      <div className="nav-bar">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <Link to="/" style={{ textDecoration: 'none' }}><h2>Home</h2></Link>    
+      <h2>Help</h2>
+    </div>
+    </Router>
+    
+      
   );
 };
 export default App;
