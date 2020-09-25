16ea19aaf076e767c327ff6cf31d6ece411fa530
retroazure <tomascosta005@outlook.com>
2020-09-25 13:08:04 +0100

    Mvp tasks

--- src/App.js
+++ src/App.js
@@ -1,37 +1,84 @@
-import React from "react";
-import { BrowserRouter as Router, Route,Link, Redirect } from 'react-router-dom';
-import Form from './components/Form';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Form from "./components/Form";
+import './App.css';
+
 
 const App = () => {
+  let [formState, setFormState] = useState({
+    name: "",
+    size: "",
+    mushrooms: false,
+    onions: false,
+    feta: false,
+    garlic: false,
+    instructions: "",
+  });
 
+  const [inputErrors, setInputErrors] = useState({
+    //State for form input validation errors
+    name: "",
+    size: "",
+    mushrooms: "",
+    onions: "",
+    feta: "",
+    garlic: "",
+    instructions: "",
+  });
+  
+  const removeButton = ()=>{
+    document.querySelector(".buildButton").style.display = "none";
+  }
+  
+  const displayButton = ()=>{
+    document.querySelector(".buildButton").style.display = "inline-block";
+  }
 
   return (
     <Router>
-      <div className="nav-bar">
-      <h1>Lambda Eats</h1>
-      <Link style= {{textDecoration: 'none'}}to="/" onClick = {()=>{
-        document.querySelector('.pizza-form').style.display = "none";
-        document.querySelector('.form-invoker button').style.display = "block";
-      
-      }}><h2>Home</h2></Link>    
-      <h2>Help</h2>
-    </div>
+      <div className="App">
+        <div style={{display: 'flex'
+        , justifyContent: 'start'
+        , flexDirection: 'row'
+        , alignItems: 'center'
+        , paddingTop: '20px'}}>
 
-    <div className="form-invoker">
-      <Link style={{textDecoration: 'none'}}to="/pizza">
-      {<button style={{ textDecoration: 'none', border: 'solid 1px black', color: 'black', padding: '15px 32px', textAlign: 'center', fontSize: '16px' }} onClick={()=>{
-        document.querySelector('.pizza-form').style.display = "block";
-        document.querySelector('.form-invoker button').style.display = "none";
+          <h1 style={{paddingLeft: '150px', paddingRight: '800px'}}>Lambda Eats</h1>
+          <Link onClick={displayButton} style={{textDecoration: 'none', fontSize: '1.5rem', fontWeight: 'bold'}} to="/">Home</Link>
+          <Link style={{textDecoration: 'none',paddingLeft:'150px',fontSize: '1.5rem', fontWeight: 'bold'}} to="/">Help</Link>
+        
+      </div>
 
-      }}>Pizza?</button>}
-      </Link>
+      <div style={{paddingTop: '100px'}}>
+      <Link to="/pizza" id="buildlink">
+          <button className="buildButton" onClick={removeButton}>Build your pizza</button>
+        </Link>
+      </div>
+        
+        <Route path="/pizza">
+          <Form
+            formState={formState}
+            setFormState={setFormState}
+            inputErrors={inputErrors}
+            setInputErrors={setInputErrors}
+          />
+        </Route>
 
-     <Form/>
-    </div>
+        <Route path="/thanks">
+          <h1>Congrats! Pizza is on its way!</h1>
 
+          <img
+            src="https://media.giphy.com/media/9fuvOqZ8tbZOU/giphy.gif"
+            alt="gif"
+          />
+        </Route>
+      </div>
     </Router>
-    
-      
   );
 };
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,245 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import * as yup from 'yup';
+import axios from 'axios';
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Redirect
+  } from "react-router-dom";
+
+import { render } from "@testing-library/react";
 
 const Form = (props)=>
 {
-    return(
-       
-       <form className="pizza-form" style={{display: 'none'}}>  
-        Name:
-        <input type="text" name="name" />
-        </form>
-  
+    //Passed from App.js
+
+    let {formState, setFormState, inputErrors, setInputErrors} = props;
+
+    
+
+    //Input Validation Schema using yup
+
+    const formSchema = yup.object().shape({
+        name: yup
+            .string()
+            .min(2, "Please type your name")
+            .required("Name is Required"),
+        size: yup
+            .string(),
+        mushrooms: yup
+            .boolean(),
+        onions: yup
+            .boolean(),
+        feta: yup
+            .boolean(),
+        garlic: yup
+            .boolean(),
+        instructions: yup
+            .string()
+      });
+
+    //Functions
+
+    let onFormInput = event => {
+
+        if (event.target.type !== 'checkbox') {
+
+            setFormState({
+
+                ...formState,
+                [event.target.name]: event.target.value
+    
+            })
+
+        } else {
+
+            setFormState({
+
+                ...formState,
+                [event.target.name]: event.target.checked
+    
+            })
+
+        }
+
+
+        //Sets state of inputErrors if there is an invalid input
+
+        yup
+            .reach(formSchema, event.target.name)
+            .validate(event.target.value)
+            .then(valid => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.name]: ''
+
+                });
+
+            })
+            .catch(err => {
+
+                setInputErrors({
+
+                    ...inputErrors,
+                    [event.target.name]: err.errors[0]
+
+                });
+
+            })
+
+            formSchema.isValid(formState).then(valid => { //Enables button if the input is valid
+
+                document.querySelector('form button').disabled = !valid;
+    
+            })
+        
+        console.log(formState)
+
+        event.persist()
+
+    }
+
+    let onFormSubmit = event => {
+
+        event.preventDefault()
+
+        axios
+            .post('https://reqres.in/api/users', formState)
+            .then(response => {
+
+                console.log('Data response:', response.data);
+
+                document.querySelector('.pizzaForm').style.display = 'none';
+
+                render(
+
+                    <Router>
+                        <Redirect push to='/thanks' />
+
+                        <h1>Congrats! Pizza is on its way!</h1>
+
+                        <img src='https://media.giphy.com/media/9fuvOqZ8tbZOU/giphy.gif' alt='gif' />
+
+                    </Router>,
+                    
+                    document.querySelector('.App')
+                )
+                
+
+            })
+            .catch(err => {
+
+                console.log(err);
+
+            });
+
+    }
+
+    return (
+
+        <div className='pizzaForm'>
+
+            <h1>Build your pizza</h1>
+
+            <form onSubmit={event => onFormSubmit(event)}>
+
+                <label>
+                    Name
+
+                    <input 
+                    type='text' 
+                    name='name'
+                    onChange={event => onFormInput(event)}
+                    value={formState.name.value}>
+                    </input>
+
+                    <h3 id='name-error'>{inputErrors.name}</h3>
+
+                </label> 
+
+                <label>
+                    Pizza Size
+
+                    <select 
+                    name='size' 
+                    onChange={event => onFormInput(event)}
+                    value={formState.size.value}>
+
+                        <option>Small</option>
+                        <option>Medium</option>
+                        <option>Large</option>
+
+                    </select>
+
+                </label>     
+                
+                <h3>Toppings</h3>
+
+                <label>
+                    Mushrooms 
+
+                    <input 
+                    type='checkbox' 
+                    name='mushrooms'
+                    onChange={event => onFormInput(event)}>
+                    </input>
+
+                </label>           
+
+                <label>
+                    Onions 
+
+                    <input 
+                    type='checkbox' 
+                    name='onions'
+                    onChange={event => onFormInput(event)}>
+                    </input>
+
+                </label>    
+
+                <label>
+                    Feta cheese 
+
+                    <input 
+                    type='checkbox' 
+                    name='feta'
+                    onChange={event => onFormInput(event)}>
+                    </input>
+
+                </label>  
+
+                <label>
+                    Roasted garlic 
+
+                    <input 
+                    type='checkbox' 
+                    name='garlic'
+                    onChange={event => onFormInput(event)}>
+                    </input>
+
+                </label> 
+
+                <label>
+                    Special instructions
+
+                    <input 
+                    type='text' 
+                    name='instructions'
+                    onChange={event => onFormInput(event)}
+                    value={formState.instructions.value}>
+                    </input>
+
+                </label>
+
+                {/* <Link to='/thanks'> */}
+                    <button disabled={true}>Submit order</button>
+                {/* </Link> */}
+            </form>
+
+        </div>
     )
 }
 
